2c3c760900a6c00bced64a8badbf0f444cdb13b4
cstef <root@cstef.dev>
2025-04-02 13:28:03 +0200

    fix: use duckduckgo instead of google for favicons

--- frontend/web/src/components/LinkFavicon.tsx
+++ frontend/web/src/components/LinkFavicon.tsx
@@ -2,34 +2,45 @@ import { useState } from "react";
 import Icon from "./Icon";
 
 interface Props {
-  url: string;
+	url: string;
 }
 
 const getFaviconUrlWithProvider = (url: string, provider: string) => {
-  try {
-    const searchParams = new URLSearchParams();
-    searchParams.set("domain", new URL(url).hostname);
-    searchParams.set("sz", "64");
-    return new URL(`?${searchParams.toString()}`, provider).toString();
-  } catch (error) {
-    return "";
-  }
+	try {
+		const domain = new URL(url).hostname;
+		const faviconUrl = `${provider}/${domain}.ico`;
+		return faviconUrl;
+	} catch (error) {
+		return "";
+	}
 };
 
 const LinkFavicon = (props: Props) => {
-  const { url } = props;
-  const faviconProvider = "https://www.google.com/s2/favicons";
-  const [faviconUrl, setFaviconUrl] = useState<string>(getFaviconUrlWithProvider(url, faviconProvider));
+	const { url } = props;
+	const faviconProvider = "https://icons.duckduckgo.com/ip3";
+	const [faviconUrl, setFaviconUrl] = useState<string>(
+		getFaviconUrlWithProvider(url, faviconProvider),
+	);
 
-  const handleImgError = () => {
-    setFaviconUrl("");
-  };
+	const handleImgError = () => {
+		setFaviconUrl("");
+	};
 
-  return faviconUrl ? (
-    <img className="w-full h-auto rounded" src={faviconUrl} decoding="async" loading="lazy" onError={handleImgError} />
-  ) : (
-    <Icon.CircleSlash className="w-full h-auto text-gray-400" strokeWidth={1.5} />
-  );
+	return faviconUrl ? (
+		<img
+			className="w-full h-auto rounded"
+			src={faviconUrl}
+			decoding="async"
+			loading="lazy"
+			onError={handleImgError}
+			alt="favicon"
+		/>
+	) : (
+		<Icon.CircleSlash
+			className="w-full h-auto text-gray-400"
+			strokeWidth={1.5}
+		/>
+	);
 };
 
 export default LinkFavicon;
